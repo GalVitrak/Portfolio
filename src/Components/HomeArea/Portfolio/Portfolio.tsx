aff6229bdddda93f896fd1e00dc6128620d3a5c7
TommyChicknez <galvit25@gmail.com>
2023-03-21 14:10:05 +0200

    proofreading and layout changes

--- src/Components/HomeArea/Portfolio/Portfolio.tsx
+++ src/Components/HomeArea/Portfolio/Portfolio.tsx
@@ -17,15 +17,15 @@ function Portfolio(): JSX.Element {
         <div className="descriptionContainer">
           <div></div>
           <p>
-            This is a web system to help plan and manage vacations. You can
-            filter vacations by destination, date and price. While logged in you
-            can also follow vacations and see them in a separate page.
+            This is a web system designed to help plan and manage vacations. You
+            can filter vacations by destination, date, and price. While logged
+            in, you can also follow vacations and view them on a separate page.
           </p>
           <div></div>
         </div>
         <div className="technologiesContainer">
           <h4>Technologies</h4>
-          <span>React | Redux | Node.js | Express | MySQL | AWS  </span>
+          <span>React | Redux | Node.js | Express | MySQL | AWS </span>
         </div>
         <div className="compContainer">
           <h4>Compatibility</h4>
@@ -34,12 +34,18 @@ function Portfolio(): JSX.Element {
         <div className="linksContainer">
           <h4>Links</h4>
           <div className="linksImageContainer">
-            <a href="https://travel-planner-gv.herokuapp.com/home" target="_blank"
-            rel="noopener noreferrer">
-              <img src={website} alt="site"  />
+            <a
+              href="https://travel-planner-gv.herokuapp.com/home"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={website} alt="site" />
             </a>
-            <a href="https://github.com/GalVitrak/Travel-Planner" target="_blank"
-            rel="noopener noreferrer">
+            <a
+              href="https://github.com/GalVitrak/Travel-Planner"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={github} alt="github" />
             </a>
           </div>
@@ -56,9 +62,9 @@ function Portfolio(): JSX.Element {
         <div className="descriptionContainer">
           <div></div>
           <p>
-            This is a web system used to upload and share images and albums.
-            You can search albums by a unique code, like and comment on images.
-            You can also create albums and decide who can watch and edit them.
+            This is a web system used to upload and share images and albums. You
+            can search albums by a unique code, like and comment on images, and
+            create albums with custom viewing and editing permissions.
           </p>
           <div></div>
         </div>
@@ -73,8 +79,11 @@ function Portfolio(): JSX.Element {
         <div className="linksContainer">
           <h4>Links</h4>
           <div className="linksImageContainer">
-            <a href="https://github.com/GalVitrak/PhotoProject" target="_blank"
-            rel="noopener noreferrer">
+            <a
+              href="https://github.com/GalVitrak/PhotoProject"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img src={github} alt="github" />
             </a>
           </div>
